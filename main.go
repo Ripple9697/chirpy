eb857570bb1fbc49ab8961ec1b7ac954160de8f4
GerryO <gerald0@keemail.me>
2026-07-31 14:49:14 -0500

    wewe

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"sync/atomic"
+
+	"github.com/Ripple9697/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -35,10 +43,19 @@ func (cfg *apiConfig) handlerPrint(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
 	const filepathRoot = "."
 	const port = "8080"
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to db; %d", err)
+	}
 
-	conf := apiConfig{}
+	conf := apiConfig{
+		fileserverHits: atomic.Int32{},
+		db:             database.New(db),
+	}
 
 	fileServ := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 	mux := http.NewServeMux()
@@ -64,27 +81,44 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerValidate(w http.ResponseWriter, r *http.Request) {
+	blacklist := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
 	type Req struct {
 		Body string `json:"body"`
 	}
+	type validResp struct {
+		Valid bool `json:"valid"`
+	}
+	type CleanedResp struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
 	respReq := Req{}
 
 	err := json.NewDecoder(r.Body).Decode(&respReq)
 	if err != nil {
 		log.Printf("Error decoding parameters %s", err)
-		w.WriteHeader(500)
+		//w.WriteHeader(500)
+		respondWithError(w, 500, fmt.Sprintf("Error decoding parameters %s", err))
 		return
 	}
 	if len(respReq.Body) > 140 {
 		respondWithError(w, 400, "Chirp is too long")
 		return
-	} else {
-		type validResp struct {
-			Valid bool `json:"valid"`
-		}
-		respondWithJSON(w, 200, validResp{Valid: true})
-		return
 	}
+	words := strings.Split(respReq.Body, " ")
+	for i := range words {
+		_, banned := blacklist[strings.ToLower(words[i])]
+		if banned {
+			words[i] = "****"
+		}
+	}
+	finalString := strings.Join(words, " ")
+	fmt.Println(words)
+	respondWithJSON(w, 200, CleanedResp{CleanedBody: finalString})
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
